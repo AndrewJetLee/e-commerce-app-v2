2d173aecf646e26bc1c49575113224268d725851
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-16 18:59:15 -0600

    Finished draft styling and functionality of Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Box, Rating } from "@mui/material/";
 import { useState } from "react";
+import { Remove, Add } from "@mui/icons-material/";
 import Dropdown from "../components/Dropdown";
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
@@ -12,63 +13,85 @@ const Product = () => {
   const [rating, setRating] = useState(2);
   const [color, setColor] = useState("Beige");
   const [size, setSize] = useState("S");
+  const [count, setCount] = useState(1);
 
   const handleChangeDropdown = (event, label) => {
     if (label === "color") setColor(event.target.value);
     if (label === "size") setSize(event.target.value);
   };
 
+  const handleClickCounter = (action) => {
+    if (action === "add") {
+      count < 10 && setCount(count + 1);
+    }
+    if (action === "remove") {
+      count > 0 && setCount(count - 1);
+    }
+  };
+
   return (
-    <Container>
+    <>
       <Navbar />
       <Announcement />
-      <Content>
-        <Left>
-          <ProductImage src="https://cdn.shopify.com/s/files/1/2712/5012/products/essentials-sixth-hoodie-beige-urban-clothing-shop-659_480x480.jpg?v=1635315778"></ProductImage>
-        </Left>
+      <Container>
+        <Content>
+          <Left>
+            <ProductImage src="https://cdn.shopify.com/s/files/1/2712/5012/products/essentials-sixth-hoodie-beige-urban-clothing-shop-659_480x480.jpg?v=1635315778"></ProductImage>
+          </Left>
 
-        <Right>
-          <Info>
-            <Title>ESSENTIALS HOODIE</Title>
-            <Price>$120.00</Price>
-            <Box>
-              <Rating
-                name="productRating"
-                value={rating}
-                onChange={(e, newRating) => setRating(newRating)}
-              />
-            </Box>
-          </Info>
+          <Right>
+            <Info>
+              <Title>ESSENTIALS HOODIE</Title>
+              <Price>$120.00</Price>
+              <Box>
+                <Rating
+                  name="productRating"
+                  value={rating}
+                  onChange={(e, newRating) => setRating(newRating)}
+                />
+              </Box>
+            </Info>
 
-          <Selection>
-            <Top>
-              <Color>
-                <TopTitle>Color</TopTitle>
-                <Dropdown
-                  value={color}
-                  handleChangeDropdown={handleChangeDropdown}
-                  label={"color"}
-                  choices={ColorChoices}
-                />
-              </Color>
-              <Size>
-                <TopTitle>Size</TopTitle>
-                <Dropdown
-                  value={size}
-                  handleChangeDropdown={handleChangeDropdown}
-                  label={"size"}
-                  choices={SizeChoices}
-                />
-              </Size>
-            </Top>
-            <Bottom>
-              <Quantity></Quantity>
-              <AddToCart></AddToCart>
-            </Bottom>
-          </Selection>
-        </Right>
-      </Content>
-    </Container>
+            <Selection>
+              <Top>
+                <Color>
+                  <TopTitle>Color</TopTitle>
+                  <Dropdown
+                    value={color}
+                    handleChangeDropdown={handleChangeDropdown}
+                    label={"color"}
+                    choices={ColorChoices}
+                  />
+                </Color>
+                <Size>
+                  <TopTitle>Size</TopTitle>
+                  <Dropdown
+                    value={size}
+                    handleChangeDropdown={handleChangeDropdown}
+                    label={"size"}
+                    choices={SizeChoices}
+                  />
+                </Size>
+              </Top>
+              <Bottom>
+                <AddToCart>ADD TO CART</AddToCart>
+                <Quantity>
+                  <Remove
+                    className="removeIcon icon"
+                    onClick={() => handleClickCounter("remove")}
+                  />
+                  <Counter>{count}</Counter>
+                  <Add
+                    className="addIcon icon"
+                    onClick={() => handleClickCounter("add")}
+                  />
+                </Quantity>
+              </Bottom>
+            </Selection>
+          </Right>
+        </Content>
+      </Container>
+    </>
   );
 };
 
@@ -78,10 +101,11 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const Content = styled.div`
-  width: 80%;
+  width: 90%;
   display: flex;
 `;
 
@@ -92,12 +116,14 @@ const Left = styled.div`
 const ProductImage = styled.img`
   height: 100%;
   width: 100%;
-  object-fit: contain;
+  min-width: 400px;
+  object-fit: cover;
 `;
 
 const Right = styled.div`
   flex: 1;
   margin-left: 20px;
+  margin-top: 50px;
 `;
 
 const Title = styled.h1``;
@@ -124,6 +150,36 @@ const Color = styled.div`
 
 const Size = styled(Color)``;
 
-const Bottom = styled.div``;
-const Quantity = styled.div``;
-const AddToCart = styled.button``;
+const Bottom = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+`;
+const Quantity = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  .icon {
+    margin: 0 8px;
+  }
+`;
+
+const Counter = styled.span`
+  border: 1px solid gray;
+  height: 25px;
+  width: 25px;
+  padding: 8px;
+  border-radius: 5px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AddToCart = styled.button`
+  padding: 12px 59px;
+  background-color: teal;
+  color: white;
+  font-weight: 500;
+  font-size: 16px;
+`;
