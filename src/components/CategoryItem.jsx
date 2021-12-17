dd0b0931abc1b45baa9322f57a1292370ea4c6cd
AndrewJetLee <andrewjetlee777@gmail.com>
2021-12-17 15:51:31 -0600

    minor changes

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const CategoryItem = ({ item }) => {
   return (
@@ -23,6 +24,7 @@ const Container = styled.div`
     width: 100%;
     height: 100%;
     object-fit: cover;
+    ${mobile({ height: "30vh"})};
   }
 `;
 
