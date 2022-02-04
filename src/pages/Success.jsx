4883f1dce98405228dfb70ef1d87f9ff3712d435
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-03 18:48:01 -0600

    Finished basic styling for success page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -14,15 +14,15 @@ const Success = () => {
   console.log(location);
   return (
     <>
-      <Announcement />
       <Navbar />
+      <Announcement />
       <Container>
         <Content>
           <CheckCircleOutlineIcon className="checkIcon" />
           <OrderHeader>YOUR ORDER HAS BEEN RECEIVED</OrderHeader>
           <OrderInfo>
             <OrderInfoHeader>THANK YOU FOR YOUR PURCHASE!</OrderInfoHeader>
-            <OrderNumber>123123</OrderNumber>
+            <OrderNumber>Your order number is: 123123</OrderNumber>
             <OrderText>
               You will receive an order confirmation email with details of your
               order.
@@ -39,24 +39,44 @@ const Success = () => {
 export default Success;
 
 const Container = styled.div`
-  height: calc(100vh - 364px);
+  height: calc(100vh - 364px - 80px);
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
 `;
 const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   height: 100%;
+  justify-content: center;
   .checkIcon {
     font-size: 100px;
     color: green;
   }
 `;
 
-const OrderHeader = styled.h1``;
-const OrderInfo = styled.section``;
+const OrderHeader = styled.h1`
+    margin-top: 10px;
+`;
+
+const OrderInfo = styled.section`
+  display: flex;
+  height: 20%;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  justify-content: space-around;
+`;
 
 const OrderInfoHeader = styled.h2``;
 const OrderNumber = styled.span``;
 const OrderText = styled.p``;
-const ContinueShoppingButton = styled.button``;
+const ContinueShoppingButton = styled.button`
+    margin-top: 20px;
+    background-color: blue;
+    color: white;
+    padding: 8px 16px;
+    border-radius: 2px;
+`;
