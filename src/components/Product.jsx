67e8bb97d59fe5a9c348af649c1561d758932b58
AndrewJetLee <andrewjetlee777@gmail.com>
2022-02-06 20:44:58 -0600

    Added product title and price to each product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,71 +1,97 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
-const Product = ({item}) => {
-    const navigate = useNavigate();
-    return (
-        <Container>
-            <Image src={item.image}/>
-            <Info>
-                <button className="searchButton" onClick={() => navigate(`/product/${item._id}`)}>
-                    VIEW
-                </button>
-            </Info>
-        </Container>
-    )
-}
+const Product = ({ item }) => {
+  const navigate = useNavigate();
+  return (
+    <Container>
+      <Wrapper>
+        <Image src={item.image} />
 
-export default Product
+        <Info>
+          <button
+            className="searchButton"
+            onClick={() => navigate(`/product/${item._id}`)}
+          >
+            VIEW
+          </button>
+        </Info>
+      </Wrapper>
+      <Title>{item.title}</Title>
+      <Price>${item.price}.00</Price>
+    </Container>
+  );
+};
+
+export default Product;
 
 const Container = styled.div`
-    width: 80%;
-    padding: 20px;
-    background-color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center; 
-    position: relative; 
-`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 20px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
 
 const Image = styled.img`
-    width: 300px;
-    height: 300px; 
-    object-fit: contain; 
-`
+  width: 300px;
+  height: 300px;
+  object-fit: contain;
+`;
 const Info = styled.div`
-    position: absolute; 
-    top: 0;
-    left: 0; 
-    width: 100%;
-    height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.25);
+  opacity: 0;
+  transition: all 0.3s ease;
+  cursor: pointer;
+  button {
+    background-color: #030364;
+    padding: 18px 54px;
     display: flex;
+    align-items: center;
     justify-content: center;
-    align-items: center; 
-    background-color: rgba(0, 0, 0, 0.25); 
-    opacity: 0; 
-    transition: all .3s ease;
-    cursor: pointer; 
-    button {
-        background-color: #030364; 
-        padding: 18px 54px; 
-        display: flex; 
-        align-items: center; 
-        justify-content: center; 
-        transition: all .167s ease;
-        color: white; 
-        font-weight: 500; 
-        cursor: pointer; 
-        .icon {
-            font-size:20px; 
-        }
-        :hover {
-            transform: scale(1.1);
-        }
-    }
-    .cartButton {
-        margin-right: 15px; 
+    transition: all 0.167s ease;
+    color: white;
+    font-weight: 500;
+    cursor: pointer;
+    .icon {
+      font-size: 20px;
     }
     :hover {
-        opacity: 1;
+      transform: scale(1.1);
     }
+  }
+  .cartButton {
+    margin-right: 15px;
+  }
+  :hover {
+    opacity: 1;
+  }
+`;
+
+const Title = styled.span`
+    font-weight: 500;
+    font-size: 14px;
+`;
+
+const Price = styled.span`
+    font-size: 14px;
 `
